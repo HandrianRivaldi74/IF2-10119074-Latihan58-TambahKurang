183718d3698d0cc05f0cf4f1b095c7421deb4e17
HandrianRivaldi74 <72905859+HandrianRivaldi74@users.noreply.github.com>
2020-11-21 15:23:08 +0700

    Add files via upload

---
+++ IF2-10119074-Latihan58-TambahKurang/src/if2/pkg10119074/latihan58/tambahkurang/IF210119074Latihan58TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10119074.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Acer
+ * Nama : HANDRIAN RIVALDI
+ * Kelas : PBOIF2
+ * NIM : 10119074
+ * Deskrifsi:
+ */
+public class IF210119074Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan objJumlahBilangan = new JumlahBilangan();
+        SelisihBilangan objSelisihBilangan = new SelisihBilangan();
+
+        System.out.print("Hasil perjumahan = ");objJumlahBilangan.tampilHasilJumlah();
+        System.out.print("Hasil Selisih 3 - 4 = ");objSelisihBilangan.tampilSelisih();
+    }
+    
+}
---
+++ IF2-10119074-Latihan58-TambahKurang/src/if2/pkg10119074/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10119074.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Acer
+ * Nama : HANDRIAN RIVALDI
+ * Kelas : PBOIF2
+ * NIM : 10119074
+ * Deskrifsi:
+ */
+public class JumlahBilangan extends Bilangan {
+    public void tampilHasilJumlah(){
+        System.out.println(""+(getX()+getY()));
+    }
+    
+}
